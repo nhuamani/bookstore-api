e259fe39731f79a6a2fcbbe178990a0ccd46e563
nhuamani <nhuamani.c7@gmail.com>
2023-06-06 18:45:54 -0500

    using @ResponseStatus

--- src/main/java/com/nhuamani/bookstoreapi/HelloController.java
+++ src/main/java/com/nhuamani/bookstoreapi/HelloController.java
@@ -1,12 +1,11 @@
 package com.nhuamani.bookstoreapi;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+    @ResponseStatus(value = HttpStatus.CREATED)
     @RequestMapping(method = RequestMethod.GET, path = "/")
     public String index(@RequestParam(defaultValue = "Watson") String name) {
         return "Hello " + name + "!!!";
