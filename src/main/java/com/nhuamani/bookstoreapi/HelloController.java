2c92bc90e009843c5b341368114ef222c8a18873
nhuamani <nhuamani.c7@gmail.com>
2023-06-06 18:21:22 -0500

    using @RequestParam

--- src/main/java/com/nhuamani/bookstoreapi/HelloController.java
+++ src/main/java/com/nhuamani/bookstoreapi/HelloController.java
@@ -2,12 +2,13 @@ package com.nhuamani.bookstoreapi;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @RequestMapping(method = RequestMethod.GET, path = "/")
-    public String index() {
-        return "Hello " +  "!!!";
+    public String index(@RequestParam(defaultValue = "Watson") String name) {
+        return "Hello " + name + "!!!";
     }
 }
