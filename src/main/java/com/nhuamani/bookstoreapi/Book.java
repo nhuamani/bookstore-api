5c9914d8e3dd1aafe6364960b2856035732da7e8
nhuamani <nhuamani.c7@gmail.com>
2023-06-06 18:56:55 -0500

    using @RequestBody serialización de JSON a objeto java y objeto java a JSON

---
+++ src/main/java/com/nhuamani/bookstoreapi/Book.java
@@ -0,0 +1,23 @@
+package com.nhuamani.bookstoreapi;
+
+public class Book {
+    private String title;
+
+    private Float price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+}
--- src/main/java/com/nhuamani/bookstoreapi/HelloController.java
+++ src/main/java/com/nhuamani/bookstoreapi/HelloController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @ResponseStatus(value = HttpStatus.CREATED)
+
     @RequestMapping(method = RequestMethod.GET, path = "/")
     public String index(@RequestParam(defaultValue = "Watson") String name) {
         return "Hello " + name + "!!!";
+    }
+
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @RequestMapping(method = RequestMethod.POST, path = "/books")
+    Book createBook(@RequestBody Book book) {
+        return book;
     }
 }
